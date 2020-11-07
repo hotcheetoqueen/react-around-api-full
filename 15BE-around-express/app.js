9d10d2e96512e45c1c25f9fff9e4bd7612c1d9eb
Jessica Perelman <hotcheetoqueen@users.noreply.github.com>
2020-11-07 15:22:36 -0800

    update/remove old hardcoded middleware auth

--- 15BE-around-express/app.js
+++ 15BE-around-express/app.js
@@ -22,13 +22,6 @@ mongoose.connect('mongodb://localhost:27017/aroundb', {
   useFindAndModify: false,
 });
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '5f81ef1a30cefaa45ca45b67',
-  };
-  next();
-});
-
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 
