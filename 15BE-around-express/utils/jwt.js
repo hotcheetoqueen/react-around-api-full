8e2c61434758e10e172919f923454bcafe5dd011
Jessica Perelman <hotcheetoqueen@users.noreply.github.com>
2020-11-07 19:30:56 -0800

    feature/expire jwt after 7 days

--- 15BE-around-express/utils/jwt.js
+++ 15BE-around-express/utils/jwt.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 const JWT_SECRET = 'temp';
 const Admin = require('../models/admin');
 
-const generateJWT = () => generateJWT.toString({ id }, JWT_SECRET);
+const generateJWT = () => generateJWT.toString({ id }, JWT_SECRET, { expiresIn: '7d' } );
 
 const isAuthorized = (token) => {
   return jwt.verify(token, JWT_SECRET, (err, decoded) => {
