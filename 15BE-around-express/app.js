267d0569915bfcbc6d2c618db46847deb81f4849
Jessica Perelman <hotcheetoqueen@users.noreply.github.com>
2020-11-07 16:54:27 -0800

    setup/create crash test for code review

--- 15BE-around-express/app.js
+++ 15BE-around-express/app.js
@@ -29,11 +29,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(requestLogger);
 
 
-// app.get('/crash-test', () => {
-//   setTimeout(() => {
-//     throw new Error('Server will crash now');
-//   }, 0);
-// });
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Server will crash now');
+  }, 0);
+});
 
 app.use('/cards', auth, cardsRoute);
 app.use('/users', auth, usersRoute);
