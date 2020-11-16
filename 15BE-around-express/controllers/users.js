a7b9536d6775faa4e8a34a498cc7d80eb55b7411
Jessica Perelman <hotcheetoqueen@users.noreply.github.com>
2020-11-15 18:43:35 -0800

    hotfix/set password to hash in new user version of register module

--- 15BE-around-express/controllers/users.js
+++ 15BE-around-express/controllers/users.js
@@ -1,10 +1,8 @@
 const User = require('../models/user');
-// const Admin = require('../models/admin');
 const bcrypt = require('bcrypt');
 const SALT_ROUND = 10;
 const jwt = require('jsonwebtoken');
 const { generateJWT } = require('../utils/jwt');
-// const RequestError = require('../errors/RequestError');
 
 const { isAuthorized } = require('../utils/jwt');
 
@@ -46,7 +44,7 @@ module.exports.createUser = (req, res, next) => {
 
   bcrypt.hash(password, 10)
     .then((hash) =>
-    User.create({ name, about, avatar, email, password })
+    User.create({ name, about, avatar, email, password: hash }))
       .then((user) => res.send({ data: user }))
       .catch((err) => {
         if (err.name === 'ValidationError') {
@@ -54,10 +52,7 @@ module.exports.createUser = (req, res, next) => {
         }
         next(err);
       })
-    )
-      // console.log('user');
-
-      // .catch(next);
+      .catch(next);
 };
 
 module.exports.updateUser = (req, res, next) => {
@@ -86,27 +81,6 @@ module.exports.updateAvatar = (req, res, next) => {
     .catch(next);
 };
 
-// module.exports.registerAdmin = (req, res) => {
-//   // req.body
-//   const { email, password, name, about, avatar } = req.body;
-
-//   if (!email || !password) return res.status(400).send({ message: 'Uh oh, something is wrong with those credentials!'});
-
-//   return bcrypt.hash(password, SALT_ROUND, (err, hash) => {
-//     return Admin.findOne({ email }).select('+password')
-//       .then(admin => {
-//         if (admin) return res.status(403).send({message: 'This email already exists'});
-
-//         return Admin.create({ email, password: hash })
-//           .then(user => {
-//             res.status(200).send(user)
-//           })
-//           .catch(err => res.status(400).send(err))
-//       })
-//   });
-// }
-
-
 module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
 
@@ -115,8 +89,10 @@ module.exports.login = (req, res, next) => {
   // TODO check id & move isPasswordValid to .then(), don't need error; req._id = user._id;
 
   User.findOne({ email }).select('+password')
-    .then(user => {
+    .then((user) => {
       if (!user) {
+        console.log(user);
+
         throw new RequestError('Something is wrong with those credentials');
     } else {
       req._id = user._id;
@@ -125,7 +101,7 @@ module.exports.login = (req, res, next) => {
   })
 
   return bcrypt.compare(password, user.password)
-      .then((isPasswordValid) => {
+      .then((isPasswordValid, user) => {
         if(!isPasswordValid) {
           throw new RequestError('Something is wrong with those credentials');
         }
@@ -134,10 +110,10 @@ module.exports.login = (req, res, next) => {
         // const token = jwt.sign({ _id: req._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
         // res.header('authorization', `Bearer ${token}`);
         // res.cookie('token', token, { httpOnly: true });
-        // res.status(200).send({ token });
+        res.status(200).send({ token });
 
         //Do I still need this?
-        return res.status(200).send({ email });
+        // return res.status(200).send({ email });
       })
   .catch(next);
 }
